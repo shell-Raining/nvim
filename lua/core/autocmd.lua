298538df1aab948a87db786ddc034931bad7addb
shell-Raining <shellRaining@gmail.com>
2023-01-04 22:10:33 +0800

    format and add autocmd

---
+++ lua/core/autocmd.lua
@@ -0,0 +1,60 @@
+local options = require("core.options")
+
+-- auto save buffer
+if options.autoSave then
+    vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+        pattern = { "*" },
+        command = "silent! wall",
+        nested = true,
+    })
+end
+
+-- auto reload log file
+if options.autoReload then
+    vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+        pattern = { "*" },
+        callback = function()
+            if vim.fn.mode() ~= "c" then
+                vim.cmd([[checktime]])
+            end
+        end,
+    })
+end
+
+-- switch input method
+if options.autoSwitchInput then
+    vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+        pattern = { "*" },
+        callback = function()
+            local input_status = tonumber(vim.fn.system("fcitx5-remote"))
+            if input_status == 2 then
+                vim.fn.system("fcitx5-remote -c")
+            end
+        end,
+    })
+end
+
+-- auto restore cursor position
+if options.autoRestoreCursePostion then
+    vim.api.nvim_create_autocmd("BufReadPost", {
+        pattern = { "*" },
+        callback = function()
+            if vim.fn.line("'\"") > 0 and vim.fn.line("'\"") <= vim.fn.line("$") then
+                vim.fn.setpos(".", vim.fn.getpos("'\""))
+                -- how do I center the buffer in a sane way??
+                -- vim.cmd('normal zz')
+                vim.cmd("silent! foldopen")
+            end
+        end,
+    })
+end
+
+-- remove auto-comments
+if options.auto_remove_new_lines_comment then
+    vim.api.nvim_create_autocmd({ "BufEnter" }, {
+        pattern = { "*" },
+        callback = function()
+            vim.opt.formatoptions = vim.opt.formatoptions - { "c", "r", "o" }
+        end,
+    })
+end
---
+++ lua/core/settings.lua
@@ -0,0 +1,59 @@
+-- this file is not a module, but a script to set options for nvim 
+
+local global = require('core.global')
+
+local function loadOptions()
+	local globalLocal = {
+		-- ui
+		ruler = false,
+		showmode = false,
+		cursorline = true,
+		numberwidth = 2,
+		number = true,
+		relativenumber = true,
+		termguicolors = true,
+		errorbells = true,
+		visualbell = true,
+		foldmethod = 'indent',
+		foldcolumn = '0',
+
+		-- edit
+		smarttab = true,
+		spell = true,
+		spelllang = 'en_us,cjk',
+		backspace = 'indent,eol,start',
+		ignorecase = true,
+		smartcase = true,
+		
+		-- buffer
+		hidden = true,
+		backup = false,
+		swapfile = false,
+
+		-- setting
+		encoding = 'utf-8',
+		scrolloff = 21,
+	}
+
+	local function isEmpty(s)
+		return s == nil or s == ""
+	end
+
+	local condaPrefix = os.getenv("CONDA_PREFIX")
+	if not isEmpty(condaPrefix) then
+		vim.g.python_host_prog = condaPrefix .. "/bin/python"
+		vim.g.python3_host_prog = condaPrefix .. "/bin/python"
+	elseif global.isMac then
+		vim.g.python_host_prog = "/usr/bin/python"
+		vim.g.python3_host_prog = "/usr/local/bin/python3"
+	else
+		vim.g.python_host_prog = "/usr/bin/python"
+		vim.g.python3_host_prog = "/usr/bin/python3"
+	end
+
+	for name, value in pairs(globalLocal) do
+		vim.o[name] = value
+	end
+end
+
+loadOptions()
