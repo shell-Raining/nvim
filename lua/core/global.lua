d97eea94c36962019cea07d31c50be7273901d73
shell-Raining <shellRaining@gmail.com>
2023-01-04 20:30:29 +0800

    update global.lua

--- lua/core/global.lua
+++ lua/core/global.lua
@@ -10,6 +10,8 @@ function global:loadVars()
 
 	local pathSeq = self.isWindows and '\\' or '/'
 	local home = self.isWindows and os.getenv('USERPROFILE') or os.getenv('HOME')
+	
+	self.pathSeq = pathSeq
 	self.cacheDir = home .. pathSeq .. ".cache" .. pathSeq .. "nvim" .. pathSeq
 	self.modulesDir = self.vimPath .. pathSeq .. "modules"
 	self.home = home
