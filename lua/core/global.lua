e8c2f4f3f205fb9d461b2c637174b02b9871fcf1
shell-Raining <shellRaining@gmail.com>
2023-01-04 12:56:55 +0800

    add mapping

--- lua/core/global.lua
+++ lua/core/global.lua
@@ -7,6 +7,13 @@ function global:loadVars()
 	self.isWindows = (osName == "Windows_NT")
 	self.isWSL = (vim.fn.has('wsl') == 1)
 	self.vimPath = vim.fn.stdpath('config')
+
+	local pathSeq = self.isWindows and '\\' or '/'
+	local home = self.isWindows and os.getenv('USERPROFILE') or os.getenv('HOME')
+	self.cacheDir = home .. pathSeq .. ".cache" .. pathSeq .. "nvim" .. pathSeq
+	self.modulesDir = self.vimPath .. pathSeq .. "modules"
+	self.home = home
+	self.dataDir = string.format('%s/site/', vim.fn.stdpath('data')) -- TODO: how to use
 end
 
 global:loadVars()
---
+++ lua/core/mapping.lua
@@ -0,0 +1,24 @@
+local bind = require('keymap.bind')
+local mapCR = bind.mapCR
+local mapCmd = bind.mapCmd
+
+local defMap = {
+	['n|<leader><esc>'] = mapCR('wqa!'):withNoremap():withSilent(),
+	['i|jj'] = mapCmd('<esc>'):withSilent(),
+	['n|<esc>'] = mapCR('noh'):withSilent(),
+	['n|<c-u>'] = mapCR("normal! " .. math.ceil(vim.api.nvim_win_get_height(0) / 4) .. "k"):withSilent(),
+	['n|<c-d>'] = mapCR("normal! " .. math.ceil(vim.api.nvim_win_get_height(0) / 4) .. "j"):withSilent(),
+	['n|<leader>cs'] = mapCR('set spell!'):withSilent(),
+	['c|<m-p>'] = mapCmd('<up>'):withSilent(),
+	['c|<m-n>'] = mapCmd('<down>'):withSilent(),
+	['nx|k'] = mapCmd(function() return vim.v.count > 0 and "k" or "gk" end):withSilent():withExpr(),
+	['nx|j'] = mapCmd(function() return vim.v.count > 0 and "j" or "gj" end):withSilent():withExpr(),
+	['nx|H'] = mapCmd(function() return vim.v.count > 0 and "^" or "g^" end):withSilent():withExpr(),
+	['nx|L'] = mapCmd(function() return vim.v.count > 0 and "$" or "g$" end):withSilent():withExpr(),
+	['ict|<m-k>'] = mapCmd("<up>"),
+	['ict|<m-j>'] = mapCmd("<down>"),
+	['ict|<m-h>'] = mapCmd("<left>"),
+	['ict|<m-l>'] = mapCmd("<right>"),
+}
+
+bind.nvimLoadMapping(defMap)
