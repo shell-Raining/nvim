c4926003c9aa5eb872131b2744c059ff98bdc00c
shell-Raining <shellRaining@gmail.com>
2023-01-05 00:15:30 +0800

    refactor log module

--- lua/log/log.lua
+++ lua/log/log.lua
@@ -7,25 +7,27 @@ local M = {}
 
 local logInfoPath = vimPath .. pathSeq .. 'log'
 
-function M:writeLog(path, info)
-	local file = io.open(path, 'a')
-	file:write(os.date("%Y-%m-%d %H-%M-%S ", os.time()))
-	file:write(info .. '\n')
-	file:close()
+function M.writeLog(path, info)
+	local fd = io.open(path, 'a')
+	if not fd then
+		print(string.format('fd %s not opened because not have this dir', path))
+		return
+	end
+
+	fd:write(os.date("%Y-%m-%d %H:%M:%S ", os.time()))
+	if not info then
+		fd:write('nil info, please check\n')
+		return
+	end
+	fd:write(info .. '\n')
+	fd:close()
 end
 
+-- write log info into default dir and file ( tag with date )
 function M.log(info)
-	local filename = os.date("%Y%m%d")
+	local filename = os.date("%Y%m%d.log")
 	local path = logInfoPath .. pathSeq .. 'default' .. pathSeq .. filename
-	local fd = io.open(path, 'a')
-
-	if fd then
-		fd:write(os.date("%Y-%m-%d %H:%M:%S ", os.time()))
-		fd:write(info .. '\n')
-		fd:close()
-	else
-		print(string.format('fd %s not opened', path))
-	end
+	M.writeLog(path, info)
 end
 
 return M
