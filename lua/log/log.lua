dc6d778ceb3c52f3678946d9c5167f9ad8fe2d0f
shell-Raining <shellRaining@gmail.com>
2023-01-05 13:11:48 +0800

    update log table func

--- lua/log/log.lua
+++ lua/log/log.lua
@@ -1,11 +1,12 @@
 -- this is a lua module that used to print log
 local glob = require('core.global')
+local serial = require('utils.serial')
 local vimPath = glob.vimPath
 local pathSeq = glob.pathSeq
 
 local M = {}
 
-local logInfoPath = vimPath .. pathSeq .. 'log'
+local logInfoDir = vimPath .. pathSeq .. 'log'
 
 function M.writeLog(path, info)
 	local fd = io.open(path, 'a')
@@ -23,11 +24,25 @@ function M.writeLog(path, info)
 	fd:close()
 end
 
--- write log info into default dir and file ( tag with date )
+--[[
+write log info into default dir and file ( tag with date )
+note: you need add \n manually
+]]--
 function M.log(info)
 	local filename = os.date("%Y%m%d.log")
-	local path = logInfoPath .. pathSeq .. 'default' .. pathSeq .. filename
-	M.writeLog(path, info)
+	local defaultPath = logInfoDir .. pathSeq .. 'default' .. pathSeq .. filename
+	M.writeLog(defaultPath, info)
+end
+
+--[[ 
+logTable can write a table info into log
+name is just a string to convenient to distinguishing name
+skipNewLine is a boolean value to configure whether use \n to split items 
+note: logTable will add \n automatically
+]]--
+function M.logTable(table, name, skipNewLine)
+	local info = serial.serializeTable(table, name, skipNewLine) 
+	M.log(info)
 end
 
 return M
---
+++ lua/utils/serial.lua
@@ -0,0 +1,40 @@
+-- this module contains some serial function (from other structure to string)
+
+local M = {}
+
+--[[
+serializeTable can make a table to string
+name is just a string to convenient to distinguishing name
+skipNewLines configure whether use \n to split items 
+depth maybe you can not set, or use 0
+]]--
+function M.serializeTable(val, name, skipNewLines, depth)
+    skipNewLines = skipNewLines or false
+    depth = depth or 0
+
+    local tmp = string.rep(" ", depth)
+
+    if name then tmp = tmp .. name .. " = " end
+
+    if type(val) == "table" then
+        tmp = tmp .. "{" .. (not skipNewLines and "\n" or "")
+
+        for k, v in pairs(val) do
+            tmp =  tmp .. M.serializeTable(v, k, skipNewLines, depth + 1) .. "," .. (not skipNewLines and "\n" or "")
+        end
+
+        tmp = tmp .. string.rep(" ", depth) .. "}"
+    elseif type(val) == "number" then
+        tmp = tmp .. tostring(val)
+    elseif type(val) == "string" then
+        tmp = tmp .. string.format("%q", val)
+    elseif type(val) == "boolean" then
+        tmp = tmp .. (val and "true" or "false")
+    else
+        tmp = tmp .. "\"[inserializeable datatype:" .. type(val) .. "]\""
+    end
+
+    return tmp
+end
+
+return M
