7aec159bf2454ef4afe37d41c0b6c43193cf5010
shell-Raining <shellRaining@gmail.com>
2023-01-05 17:48:10 +0800

    add string and plugin utils

---
+++ lua/utils/plugin.lua
@@ -0,0 +1,35 @@
+local optionsM = require('core.options')
+local pathUtil = require('utils.path')
+local stringUtil = require('utils.string')
+
+local endWith = stringUtil.endWith
+
+
+
+local M = {}
+
+M.loadCwdPlug = function(path)
+	-- because we do not search util path but caller function location, so use stack level 3
+	local pwd = pathUtil.getPwd(3) 
+	local notLoadPlug = {}
+
+	-- not load configured plugin
+	for _, v in pairs(optionsM.unloadPlugins) do
+		notLoadPlug[v] = true
+	end
+
+	-- load plugin in configured path
+	local cwdFileList = vim.fn.readdir(pwd)
+	for _, fname in pairs(cwdFileList) do
+		if not endWith(fname, ".lua") then goto continue end
+		local pluginName = fname:sub(1, #fname - #'.lua')
+
+		if notLoadPlug[pluginName] then goto continue end
+		local file = path .. '.' .. pluginName
+		require(file)
+
+		::continue::
+	end
+end
+
+return M
---
+++ lua/utils/string.lua
@@ -0,0 +1,11 @@
+local M = {}
+
+M.startWith = function(str, start)
+  return str:sub(1, #start) == start
+end
+
+M.endWith = function(str, ending)
+  return ending == "" or str:sub(- #ending) == ending
+end
+
+return M
